d42b5ab4331b130378f4f86243399bcab2be3f32
Justin Stone <justinstone@Justins-MacBook-Pro-2.local>
2022-05-24 13:56:32 -0400

    Made another fail check incase the user does not enter in a valid yes or no to send them back to the start, after implementing this js recommended refactoring because of too many if statements

--- Develop/script.js
+++ Develop/script.js
@@ -11,7 +11,7 @@ function generatePassword() {
   var inputLength = parseInt(prompt("Choose a length between 8 and 128"));
 
   if (inputLength >= 8 && inputLength <= 128) {
-      console.log("placeholder");
+    console.log("placeholder");
   } else {
     alert("Password to short!");
   }
@@ -19,7 +19,7 @@ function generatePassword() {
   // Prompts user for the following password preferences
   var inputLower = prompt("Would you like to include lowercase letters? Enter Yes or No");
   inputLower = inputLower[0].toLowerCase();
-  
+
   var inputUpper = prompt("Would you like to include uppercase letters? Enter Yes or No");
   inputUpper = inputUpper[0].toLowerCase();
 
@@ -32,35 +32,43 @@ function generatePassword() {
   var password = '';
   var userPreference = '';
 
-  // Checks user input and sets the userPreference string as such
-  if (inputLower == 'n' && inputUpper == 'n' && inputNumbers == 'n' && inputSpecial == 'n'){
-    alert("You have to have some type of character!")
+  // Check to make sure user did not answer no to every prompt
+  if (inputLower == 'n' && inputUpper == 'n' && inputNumbers == 'n' && inputSpecial == 'n') {
+    alert("You can't have a password with no characters!")
     generatePassword();
   }
+
+  // Check to make sure user entered valid answers for each prompt
+
+  if ((inputLower != 'y' && inputLower != 'n') || (inputUpper != 'y' && inputUpper != 'n') || (inputNumbers != 'y' && inputNumbers != 'n') || (inputSpecial != 'y' && inputSpecial != 'n')) {
+    alert("Invalid: Enter either Yes or No")
+    generatePassword();
+  }
+
   // Checks all cases and adds the string if user input == 'y'
 
-  if (inputLower == 'y'){
+  if (inputLower == 'y') {
     userPreference += lowercase;
   }
-  if (inputUpper == 'y'){
+  if (inputUpper == 'y') {
     userPreference += uppercase;
   }
-  if(inputNumbers == 'y'){
+  if (inputNumbers == 'y') {
     userPreference += numbers;
   }
-  if(inputSpecial == 'y'){
+  if (inputSpecial == 'y') {
     userPreference += special;
   }
   //creates random password, based on the users preferred length, with the new 
   // user preference string determined by the users inputs
 
-  for (var i = 0; i < inputLength; i++){
+  for (var i = 0; i < inputLength; i++) {
     var randomNumber = Math.floor(Math.random() * userPreference.length);
     password += userPreference.substring(randomNumber, randomNumber + 1);
   }
   // alerts and returns password in the box
-  
-  alert(password);
+
+  alert("Password:    " + password);
   return password;
 }
 

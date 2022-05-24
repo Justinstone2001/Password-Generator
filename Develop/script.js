2b3ad10f07b7f15c2e41d004619e670ae693bc52
Justin Stone <justinstone@Justins-MacBook-Pro-2.local>
2022-05-24 18:44:36 -0400

    Final changes and fixed bugs for two specific cases

--- Develop/script.js
+++ Develop/script.js
@@ -2,80 +2,83 @@
 var generateBtn = document.querySelector("#generate");
 
 // Instructor Provided Template: Anthony Cooper
+
 function generatePassword() {
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var numbers = "0123456789";
-  var special = "#$%&()*+,-./:;<=>?";
-  var uppercase = lowercase.toUpperCase();
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    var numbers = "0123456789";
+    var special = "#$%&()*+,-./:;<=>?";
+    var uppercase = lowercase.toUpperCase();
 
-  var inputLength = parseInt(prompt("Choose a length between 8 and 128"));
+    var inputLength = parseInt(prompt("Choose a length between 8 and 128"));
 
-  if (inputLength >= 8 && inputLength <= 128) {
-    console.log("placeholder");
-  } else {
-    alert("Password to short!");
-  }
+    if (inputLength >= 8 && inputLength <= 128) {
+        console.log(" "); // do nothing
+    } else {
+        alert("Password too short!");
+        return " ";
+    }
 
-  // Prompts user for the following password preferences
-  var inputLower = prompt("Would you like to include lowercase letters? Enter Yes or No");
-  inputLower = inputLower[0].toLowerCase();
+    // Prompts user for the following password preferences
+    var inputLower = prompt("Would you like to include lowercase letters? Enter Yes or No");
+    inputLower = inputLower[0].toLowerCase();
 
-  var inputUpper = prompt("Would you like to include uppercase letters? Enter Yes or No");
-  inputUpper = inputUpper[0].toLowerCase();
+    var inputUpper = prompt("Would you like to include uppercase letters? Enter Yes or No");
+    inputUpper = inputUpper[0].toLowerCase();
 
-  var inputNumbers = prompt("Would you like to include numbers? Enter Yes or No");
-  inputNumbers = inputNumbers[0].toLowerCase();
+    var inputNumbers = prompt("Would you like to include numbers? Enter Yes or No");
+    inputNumbers = inputNumbers[0].toLowerCase();
 
-  var inputSpecial = prompt("Would you like to include special characters? Enter Yes or No");
-  inputSpecial = inputSpecial[0].toLowerCase();
+    var inputSpecial = prompt("Would you like to include special characters? Enter Yes or No");
+    inputSpecial = inputSpecial[0].toLowerCase();
 
-  var password = '';
-  var userPreference = '';
+    //   Set variables
 
-  // Check to make sure user did not answer no to every prompt
-  if (inputLower == 'n' && inputUpper == 'n' && inputNumbers == 'n' && inputSpecial == 'n') {
-    alert("You can't have a password with no characters!")
-    generatePassword();
-  }
+    var password = '';
+    var userPreference = '';
 
-  // Check to make sure user entered valid answers for each prompt
+    // Check to make sure user did not answer no to every prompt
+    if (inputLower == 'n' && inputUpper == 'n' && inputNumbers == 'n' && inputSpecial == 'n') {
+        alert("You can't have a password with no characters!")
+    }
 
-  if ((inputLower != 'y' && inputLower != 'n') || (inputUpper != 'y' && inputUpper != 'n') || (inputNumbers != 'y' && inputNumbers != 'n') || (inputSpecial != 'y' && inputSpecial != 'n')) {
-    alert("Invalid: Enter either Yes or No")
-    generatePassword();
-  }
+    // Check to make sure user entered valid answers for each prompt
 
-  // Checks all cases and adds the string if user input == 'y'
+    if ((inputLower != 'y' && inputLower != 'n') || (inputUpper != 'y' && inputUpper != 'n') || (inputNumbers != 'y' && inputNumbers != 'n') || (inputSpecial != 'y' && inputSpecial != 'n')) {
+        alert("Invalid: Enter either Yes or No")
+    }
 
-  if (inputLower == 'y') {
-    userPreference += lowercase;
-  }
-  if (inputUpper == 'y') {
-    userPreference += uppercase;
-  }
-  if (inputNumbers == 'y') {
-    userPreference += numbers;
-  }
-  if (inputSpecial == 'y') {
-    userPreference += special;
-  }
-  //creates random password, based on the users preferred length, with the new 
-  // user preference string determined by the users inputs
+    // Checks all cases and adds the string if user input == 'y'
 
-  for (var i = 0; i < inputLength; i++) {
-    var randomNumber = Math.floor(Math.random() * userPreference.length);
-    password += userPreference.substring(randomNumber, randomNumber + 1);
-  }
-  // alerts and returns password in the box
+    if (inputLower == 'y') {
+        userPreference += lowercase;
+    }
+    if (inputUpper == 'y') {
+        userPreference += uppercase;
+    }
+    if (inputNumbers == 'y') {
+        userPreference += numbers;
+    }
+    if (inputSpecial == 'y') {
+        userPreference += special;
+    }
 
-  alert("Password:    " + password);
-  return password;
+    //creates random password, based on the users preferred length, with the new 
+    // user preference string determined by the users inputs
+
+    for (var i = 0; i < inputLength; i++) {
+        var randomNumber = Math.floor(Math.random() * userPreference.length);
+        password += userPreference.substring(randomNumber, randomNumber + 1);
+    }
+    // alerts and returns password in the box
+
+    alert("Password:    " + password);
+    return password;
 }
 
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 }
 
 // Add event listener to generate button
